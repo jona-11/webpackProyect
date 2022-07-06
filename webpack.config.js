7b07a346913c29d7381ec97df3ad22b2da30a76a
jona-11 <jonathangalimberti@hotmail.com>
2022-07-06 13:56:44 -0300

    se agrega configuracion para html y css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
  const path = require('path');
+ const htmlWebpackPlugin = require('html-webpack-plugin');
+ const miniCssExtractPlugin= require('mini-css-extract-plugin');    
 
  module.exports ={
     entry: './src/index.js',
@@ -17,7 +19,20 @@
                 loader: "babel-loader"
             },
 
-        }],
+        },
+    {
+        test: /\.css| .styl$/i,
+        use:[miniCssExtractPlugin.loader,'css-loader','stylus-loader'],
+    }
+    ],
 
     },
+    plugins: [
+        new htmlWebpackPlugin ({
+            inject: true,
+            template: "./public/index.html",
+            filename: "./index.html"
+        }),
+        new miniCssExtractPlugin (),
+    ],
  }
